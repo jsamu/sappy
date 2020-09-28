091714481c5bf8175f0f95da603d855f7727b2af
jsamu <jsamuel@ucdavis.edu>
2020-09-27 23:03:56 -0700

    axes bug fix and rms namin

--- m31_like_planes.py
+++ m31_like_planes.py
@@ -72,19 +72,23 @@ def rand_rms_min(
             rms_minor_n[n] = np.sqrt(np.nanmean(sat_prime_coords[:,2]**2))
             rms_major_n[n] = np.sqrt(np.nanmean(sat_prime_coords[:,0]**2))
 
-    min_rms_minor = np.min(rms_minor_n)
-    min_index = np.where(rms_minor_n == np.min(rms_minor_n))[0][0]
     if return_ax:
+        min_rms_minor = np.nanmin(rms_minor_n)
+        min_index = np.where(rms_minor_n == np.nanmin(rms_minor_n))[0][0]
         rms_major = rms_major_n[min_index]
         # return just the vector normal to the plane
         min_ax = rot_vecs[min_index][2]
         return {'rms_minor':min_rms_minor, 'rms_major':rms_major, 'ax':min_ax}
     elif return_parallel:
+        min_rms_minor = np.nanmin(rms_minor_n)
+        min_index = np.where(rms_minor_n == np.nanmin(rms_minor_n))[0][0]
         rms_major = rms_major_n[min_index]
         return {'rms_minor':min_rms_minor, 'rms_major':rms_major}
     elif return_dist:
         return rms_minor_n
     else:
+        min_rms_minor = np.nanmin(rms_minor_n)
+        min_index = np.where(rms_minor_n == np.nanmin(rms_minor_n))[0][0]
         return min_rms_minor
 
 def rand_angle(
@@ -175,7 +179,7 @@ def axis_ratio(
             axis_ratio_n[n] = np.nan
         else:
             sat_axes = ut.coordinate.get_principal_axes(sat_prime_coords)
-            axis_ratio_n[n] = sat_axes[2][0]
+            axis_ratio_n[n] = sat_axes[1][0]
     if return_dist:
         return axis_ratio_n
     else:
