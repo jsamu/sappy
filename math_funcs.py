157df0f06e46b9500cd9802a47db8414d379fa0e
jsamu <jsamuel@ucdavis.edu>
2022-03-30 17:51:34 -0500

    added rotation function from 2 vectors

--- math_funcs.py
+++ math_funcs.py
@@ -3,6 +3,20 @@ import math
 from scipy import stats
 
 
+def rotation_matrix_from_vectors(vec1, vec2):
+    """ Find the rotation matrix that aligns vec1 to vec2
+    :param vec1: A 3d "source" vector
+    :param vec2: A 3d "destination" vector
+    :return mat: A transform matrix (3x3) which when applied to vec1, aligns it with vec2.
+    """
+    a, b = (vec1 / np.linalg.norm(vec1)).reshape(3), (vec2 / np.linalg.norm(vec2)).reshape(3)
+    v = np.cross(a, b)
+    c = np.dot(a, b)
+    s = np.linalg.norm(v)
+    kmat = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
+    rotation_matrix = np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2))
+    return rotation_matrix
+
 def qf_stats(qf, mstar_bins=np.array([1e5, 1e6, 1e7, 1e8, 1e9]), percentile_limits=[16,84], name=''):
     std_ = np.nanstd(qf, axis=0)
     # eliminate values of scatter that are plotted outside [0,1]
