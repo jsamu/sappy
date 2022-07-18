fixed bug in surface density calculation

gas_content.py
@@ -205,9 +205,9 @@ class GalaxyGas():
         Get a broad measure of gas surface density within a subhalo(s).
         """
         sat_gas_surface_density = []
-        for sat in self.sat_gas_ind:
+        for sat, gas_rad in zip(self.sat_gas_ind, self.gas_radius):
             mass_ = np.sum(gas_particle_data['mass'][sat])
-            surface_area = np.pi*self.gas_radius**2
+            surface_area = np.pi*self.gas_rad**2
             sat_gas_surface_density.append(mass_/surface_area)
         return np.array(sat_gas_surface_density)
 

satellite_io.py
@@ -1902,15 +1902,15 @@ def convert_snapshot_to_time(
     if type(snapshot) == int:
         snapshot_mask = time_table[pre_convert_type] == snapshot
         converted_time = time_table[time_kind][snapshot_mask].values[0]
+        return converted_time
     elif (type(snapshot) == list )|(type(snapshot) == np.ndarray):
         converted_time = []
         for snap in snapshot:
             snapshot_mask = time_table[pre_convert_type] == snap
             converted_time.append(time_table[time_kind][snapshot_mask].values[0])
+        return np.array(converted_time)
     else:
         raise ValueError('Data type of input snapshot not recognized. Must be int or list/array.')
-        
-    return converted_time
 
 def color_cycle(cycle_length=14, cmap_name='plasma', low=0, high=1):
     cmap=plt.get_cmap(cmap_name)